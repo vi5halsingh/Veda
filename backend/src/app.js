ad306a36833830c6a9eafe4ef1aad6f8cf889c27
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-09-12 00:31:11 +0530

    serving public to github

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./db/db");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
 connectDB();
 app.use(
   cors({
@@ -17,5 +19,9 @@ const chatRoutes = require("./routes/chat.routes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+
+app.get('*name', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+})
 
 module.exports = app;
