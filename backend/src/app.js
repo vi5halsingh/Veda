c7d4fa3be49740e101ed2e23e7cccbad5438e942
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-09-12 00:42:19 +0530

    cors error in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 connectDB();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://veda-kx60.onrender.com"],
     credentials: true,
   })
 );
