e16008ead645b111f1f98b2d7b5361c97e29e981
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-09-13 22:37:41 +0530

    feat:PWA implementation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+    registerType: 'prompt',
+    workbox: {
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+            {
+                urlPattern: "*",
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'veda-cache',
+                }
+            },
+            
+        ]
+    }
+})
 
 createRoot(document.getElementById('root')).render(
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  VitePWA({
+    manifest: {
+      name: "veda",
+      short_name: 'AI Assistant',
+      description: 'veda is AI assistant by @dino4dev',
+      theme_color: '#000000',
+      background_color: '#ffffff',
+      icons: [
+        {
+          "src": "/logo.svg",
+          "sizes": "any",
+          "type": "image/svg+xml"
+        }
+      ],
+      start_url: '.',
+      display: 'standalone',
+     
+    },
+    registerType: 'prompt'
+  })
+  ],
 })
